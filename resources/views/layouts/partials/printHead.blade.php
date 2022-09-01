98573c9efab515834dcebf06cb3442f1897895d4
Mahmudur Rahman <rmananda007@gmail.com>
2022-09-01 10:05:22 +0600

    work on printed head

--- resources/views/layouts/partials/printHead.blade.php
+++ resources/views/layouts/partials/printHead.blade.php
@@ -1,8 +1,10 @@
-<div class="pb-2 print-show print-header border-bottom">
-    <div class="text-center">
-        <h1 class="mb-1">FOP'S</h1>
-        <h4 class="mb-1">TAILOR'S & FABRICS</h4>
-        <p class="mb-1">16, J.C. Goho Road, Railwaay Station 2no Gate, Mymensingh</p>
-        <p class="fs-6"><strong>Mobile : </strong>+880 01799 750929</p>
+<div class="print-show print-header border-bottom mb-3 pb-2">
+    <div class="d-flex align-items-center">
+    <img src="{{asset('/images/logos/icon-logo.svg')}}" height="60px" alt="">
+    <div class="border-start ps-3 ms-3">
+        <h4>MaxSOP</h4>
+        <h6>5B Green House, 27/2 Ram Babu Road, Mymensingh</h6>
+        <p class="mb-0"><strong>Phone:</strong>+880 1786 494650, +880 1786 494650</p>
+    </div>
     </div>
 </div>
